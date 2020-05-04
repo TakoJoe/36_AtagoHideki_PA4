256dba4b4d547f25e7f349fb0cd1a269f3a5f71c
TakoJoe <55574951+TakoJoe@users.noreply.github.com>
2020-05-04 18:13:28 +0800

    New Version

---
+++ 36_AtagoHideki_PA04/Assets/Scripts/LoadScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScript : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 1f;
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
